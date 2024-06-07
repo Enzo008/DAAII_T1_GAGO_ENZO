b4f0cf8427ce013b290875ed94fb61835de7588a
Enzo008 <enzoaguirre629@gmail.com>
2024-06-07 16:42:51 -0500

    Initial commit

---
+++ src/test/java/pe/edu/cibertec/DAAII_T1_GAGO_ENZO/DaaiiT1GagoEnzoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_GAGO_ENZO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaaiiT1GagoEnzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
